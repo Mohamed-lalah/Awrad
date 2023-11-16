9d595e8d29abbbf7393eda406fc41d1e0da76abf
Mohamed-lalah <mohamedlalah918@gmail.com>
2023-11-17 00:46:53 +0200

    Setting environment to add sharedPreference to the application

--- lib/providers/settings_provider.dart
+++ lib/providers/settings_provider.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class SettingProvider  extends ChangeNotifier{
 
