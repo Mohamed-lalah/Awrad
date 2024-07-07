0642d01ae83f053d75a528117f52d04a0fa1e879
Mohamed-lalah <mohamedlalah918@gmail.com>
2024-07-07 23:42:41 +0300

    Audio files working properly , radio tab logic done

--- lib/ui/screens/home/tabs/Radio/radio_item.dart
+++ lib/ui/screens/home/tabs/Radio/radio_item.dart
@@ -1,10 +1,13 @@
 
+import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
 import 'package:islami/data/model/RadioModel.dart';
 
 class RadioItem extends StatelessWidget {
  late  Radios radios ;
-   RadioItem({super.key, required this.radios});
+ late  AudioPlayer audioPlayer ;
+   RadioItem({super.key, required this.radios, required this.audioPlayer});
+
 
   @override
   Widget build(BuildContext context) {
@@ -12,17 +15,32 @@ class RadioItem extends StatelessWidget {
     double width= MediaQuery.of(context).size.width;
     return Column(
       children: [
-        Text(radios.name??""),
+        Text(radios.name??"", style: Theme.of(context).textTheme.bodySmall?.copyWith(
+          fontWeight: FontWeight.w600,
+          fontSize: 25
+        ),),
         SizedBox(height: height*0.1,),
         Row(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            IconButton(onPressed: (){}, icon: Icon(Icons.play_arrow, size: 40,color: Theme.of(context).primaryColor,)),
-            IconButton(onPressed: (){}, icon: Icon(Icons.pause, size: 40,color: Theme.of(context).primaryColor,))
+            IconButton(onPressed: (){
+              play();
+            }, icon: Icon(Icons.play_arrow, size: 40,color: Theme.of(context).primaryColor,)),
+            IconButton(onPressed: (){
+              stop();
+            }, icon: Icon(Icons.pause, size: 40,color: Theme.of(context).primaryColor,))
           ],
         ),
       ],
 
     );
   }
+ void play ()async{
+   await audioPlayer.play(UrlSource(radios.url??""));
+ }
+
+ void stop () async{
+    await audioPlayer.stop();
+ }
 }
+
--- lib/ui/screens/home/tabs/Radio/radio_tab.dart
+++ lib/ui/screens/home/tabs/Radio/radio_tab.dart
@@ -1,3 +1,4 @@
+import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:islami/data/api_manager.dart';
@@ -6,20 +7,42 @@ import 'package:islami/ui/screens/home/tabs/Radio/radio_item.dart';
 
 import '../../../../utilities/app_colors.dart';
 
-class RadioTab extends StatelessWidget {
+class RadioTab extends StatefulWidget {
 
+
+  @override
+  State<RadioTab> createState() => _RadioTabState();
+}
+
+class _RadioTabState extends State<RadioTab> {
+  late AudioPlayer audioPlayer;
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    audioPlayer= AudioPlayer();
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    audioPlayer.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     double height= MediaQuery.of(context).size.height;
     double width= MediaQuery.of(context).size.width;
 
+
+
     return  Scaffold(
       body: Column(
       mainAxisAlignment: MainAxisAlignment.center,
         children: [
           Spacer(),
-          Image.asset("assets/images/radio1x.png", height: height*0.2, width: width,),
+      Image.asset("assets/images/radio1x.png", height: height*0.2, width: width,),
           Spacer(),
          FutureBuilder<List<Radios>?>(future: ApiManager.getRadios(),
              builder: (context , snapshot){
@@ -30,14 +53,19 @@ class RadioTab extends StatelessWidget {
                return Center(child: CircularProgressIndicator(),);
              }
              else {
-               return ListView.builder(
-                 scrollDirection: Axis.horizontal,
-                   itemCount:  snapshot.data!.length??0,
+               return SizedBox(
+                 height:  height*0.3,
+                 child: ListView.builder(
+                   physics: PageScrollPhysics(),
+                   itemExtent: width ,
+                   scrollDirection: Axis.horizontal,
+                     itemCount:  snapshot.data!.length??0,
 
-                   itemBuilder:(context , int ){
-                     return RadioItem(radios: snapshot.data![int]);
+                     itemBuilder:(context , int ){
+                       return RadioItem(radios: snapshot.data![int], audioPlayer: audioPlayer,);
 
-                   });
+                     }),
+               );
              }
              }
          ),
